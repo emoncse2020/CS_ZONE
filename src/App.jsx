3882f2e4482676945f25c3751f19c85c63647291
emoncse2020 <emon.cse.2020@gmail.com>
2025-09-25 01:10:41 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <main>
+        <h1>noting</h1>
+      </main>
+      <Footer />
     </>
   );
 };
